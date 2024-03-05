245b5b89614af02b4ea0215b608cf8a49263fea3
Lupin1998 <1070535169@qq.com>
2024-03-05 22:51:32 +0000

    update coco 3x results

---
+++ detection/configs/moganet/mask_rcnn_moganet_b_fpn_mstrain_480-800_3x_coco.py
@@ -0,0 +1,81 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_moganet_fpn.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
+]
+
+# optimizer
+model = dict(
+    backbone=dict(
+        type='MogaNet_feat',
+        arch='base',
+        drop_path_rate=0.3,
+        init_cfg=dict(
+            type='Pretrained', 
+            checkpoint=\
+                'https://github.com/Westlake-AI/MogaNet/releases/download/moganet-in1k-weights/moganet_base_sz224_8xbs128_ep300.pth.tar',
+            ),
+        ),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 160, 320, 512],
+        out_channels=256,
+        num_outs=5))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# augmentation strategy originates from DETR / Sparse RCNN
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='AutoAugment',
+         policies=[
+             [
+                 dict(type='Resize',
+                      img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                                 (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                                 (736, 1333), (768, 1333), (800, 1333)],
+                      multiscale_mode='value',
+                      keep_ratio=True)
+             ],
+             [
+                 dict(type='Resize',
+                      img_scale=[(400, 1333), (500, 1333), (600, 1333)],
+                      multiscale_mode='value',
+                      keep_ratio=True),
+                 dict(type='RandomCrop',
+                      crop_type='absolute_range',
+                      crop_size=(384, 600),
+                      allow_negative_crop=True),
+                 dict(type='Resize',
+                      img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                                 (576, 1333), (608, 1333), (640, 1333),
+                                 (672, 1333), (704, 1333), (736, 1333),
+                                 (768, 1333), (800, 1333)],
+                      multiscale_mode='value',
+                      override=True,
+                      keep_ratio=True)
+             ]
+         ]),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+data = dict(
+    train=dict(pipeline=train_pipeline))
+
+# optimizer
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0002, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg=dict(custom_keys={'layer_scale': dict(decay_mult=0.),
+                                                 'scale': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+optimizer_config = dict(grad_clip=None)
+
+lr_config = dict(step=[27, 33])
+runner = dict(type='EpochBasedRunner', max_epochs=36)
+
+evaluation = dict(save_best='auto')
+checkpoint_config = dict(interval=1, max_keep_ckpts=1)
